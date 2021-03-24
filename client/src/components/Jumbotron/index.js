80127d2b6652aa141c87886b9f45e96a99e0587a
N8 <nwscott81@gmail.com>
2021-03-23 20:51:16 -0500

    pushing working Bulma Navbar, everything else busted

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -2,12 +2,20 @@ import React from "react";
 
 function Jumbotron({ children }) {
   return (
-    <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
-    >
+    // <div
+    //   style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+    //   className="jumbotron"
+    // >
+    //   {children}
+    // </div>
+    <div className="hero-body">
+    <p className="title">
       {children}
-    </div>
+    </p>
+    <p className="subtitle">
+      Primary subtitle
+    </p>
+  </div>
   );
 }
 
