6083265038bab421766cc2b57c1be11fa56e2cf7
N8 <nwscott81@gmail.com>
2021-03-29 21:56:41 -0500

    including the finale Google API to get details about a place

--- client/src/utils/googleapi.js
+++ client/src/utils/googleapi.js
@@ -1,7 +1,8 @@
-const router = require("express").Router();
-const axios = require("axios");
+import axios from "axios";
 
 export default {
+
+    // This API is going to get the search for nearby Dog Parks, based upon Location Entered
     getParkData: (location) =>
     axios({
         'method':'GET',
@@ -18,19 +19,13 @@ export default {
             'rankby':'distance',
             'location': location
         },
-    })
-};
+    }),
 
-export default {
+    // THis API is going to translate the inputted info, such as Address or Zip code, to Lat/Long info
     getZipData: () =>
     axios({
         'method':'GET',
         'url':'https://maps.googleapis.com/maps/api/geocode/json?',
-        // 'headers': {
-        //     'content-type':'application/octet-stream',
-        //     'x-rapidapi-host':'example.com',
-        //     'x-rapidapi-key': process.env.RAPIDAPI_KEY
-        // },
         'params': {
             // 'search':'parameter',
             'key':'AIzaSyAgJLBEgA0zRfF7EgTRVuBqBWahOSYLL8s',
@@ -38,5 +33,18 @@ export default {
         },
     }).then(
         getParkData(res)
-    )
+    ),
+    
+    // This API pulls up information about the place in question based upon place_id which is part fo the return for Nearby Locations
+    getDetailData: (placeId) =>
+    axios({
+        'method':'GET',
+        'url':'https://maps.googleapis.com/maps/api/geocode/json?',
+        'params': {
+            // 'search':'parameter',
+            'key':'AIzaSyAgJLBEgA0zRfF7EgTRVuBqBWahOSYLL8s',
+            'place_id': placeId,
+            'fields': 'name,formatted_address,photo,url'
+        }
+    })
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 const parkRoutes = require("./park");
-const googleApi = require("./googleapi.js");
 
 // Book routes
 router.use("/park", parkRoutes);
-router.use("/park", park)
+// router.use("/park", park)
 
 module.exports = router;
