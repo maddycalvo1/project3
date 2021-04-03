e433dc13505e605c3e72b2fb7aedfa2ab7d521dc
N8 <nwscott81@gmail.com>
2021-04-03 12:01:45 -0500

    got the first google API to work, need to pass info onto the second hop

--- routes/api/Google.js
+++ routes/api/Google.js
@@ -2,39 +2,51 @@ const axios = require("axios");
 const router = require("express").Router();
 
     // This API is going to get the search for nearby Dog Parks, based upon Location Entered
-    const getParkData = axios.get({
-        'method':'GET',
-        'url':'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=30.4332061,-97.60057859999999&rankby=distance&type=park&name=dog&key=AIzaSyAgJLBEgA0zRfF7EgTRVuBqBWahOSYLL8s',
-        // 'headers': {
-        //     'content-type':'application/octet-stream',
-        //     'x-rapidapi-host':'example.com',
-        //     'x-rapidapi-key': process.env.RAPIDAPI_KEY
-        // },
-        // 'params': {
-        //     'key':'AIzaSyAgJLBEgA0zRfF7EgTRVuBqBWahOSYLL8s',
-        //     'type':'park',
-        //     'name':'dog',
-        //     'rankby':'distance',
-        //     'location': location
-        // },
-    });
+    const getParkData = ({lat, lng}) => {
+        latLng = lat +","+ lng;
+        axios.get({
+            'method':'GET',
+            'url':'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+            // 'headers': {
+            //     'content-type':'application/octet-stream',
+            //     'x-rapidapi-host':'example.com',
+            //     'x-rapidapi-key': process.env.RAPIDAPI_KEY
+            // },
+            'params': {
+                'key':'AIzaSyAgJLBEgA0zRfF7EgTRVuBqBWahOSYLL8s',
+                'type':'park',
+                'name':'dog',
+                'rankby':'distance',
+                'location': latLng
+            },
+        })
+        .then(response => {
+            let data = response.data;
+            console.log(data);
+            res.send(data);
+        })
+        .catch(err => res.send(err));
+    };
 
     // THis API is going to translate the inputted info, such as Address or Zip code, to Lat/Long info
     router.get("/:location", (req, res, next) => {
         axios.request({
             method:'GET',
-            url:'https://maps.googleapis.com/maps/api/geocode/json?',
+            url:'https://maps.googleapis.com/maps/api/geocode/json',
             params: {
                 // 'search':'parameter',
                 'key':'AIzaSyAgJLBEgA0zRfF7EgTRVuBqBWahOSYLL8s',
-                'address': 78660
+                'address': req.params.location
             }
         })
-        .then(res => {
-            // return getParkData(res.data.results[0].geometry.location.lat +","+ res.data.results[0].geometry.location.lng)
-            // res.data.results[0].geometry.location
-            console.log(res.data.results[0].geometry.location)
-        }).catch(err => res.data(err));
+        .then(response => {
+            let data = response.data.results[0].geometry.location
+            console.log(data);
+            res.send(data)
+            console.log("banana")
+            return getParkData(data)
+            
+        }).catch(err => res.send(err));
     });
         
     // This API pulls up information about the place in question based upon place_id which is part fo the return for Nearby Locations
