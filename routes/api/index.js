56ecf71abeb8dd92abbb541734438ee1cd8876c4
maddycalvo1 <madison.calvo1@gmail.com>
2021-04-05 19:52:16 -0500

    blog routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const parkRoutes = require("./park");
 const googleRoutes = require("./Google");
+const blogRoutes = require("./blog");
 
 // Book routes
 router.use("/park", parkRoutes);
 router.use("/search", googleRoutes);
+router.use("/blog", blogRoutes);
 // router.use("/park", park)
 
 module.exports = router;
